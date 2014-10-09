8b0ef314e63fc7697b1de73b0baf213c9c61e01d
MichaelMGonzalez <mmg005@ucsd.edu>
2014-10-08 23:20:29 -0700

    Added network jazz

---
+++ Maze2D/Assets/Standard Assets/NetworkingMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkingMenu : MonoBehaviour {
+
+	public string connectionIP = "127.0.0.1";
+	public int portNumber = 8888;
+    public static bool Connected { get; private set; }
+
+	private void OnConnectedToServer()
+	{
+		Connected = true;
+	}
+
+	private void OnServerInitialized()
+	{
+		Connected = true;
+	}
+
+
+	private void OnDisconnectedFromServer()
+	{
+		Connected = false;
+	}
+
+	private void OnGUI()
+	{
+		if (!Connected)
+		{
+			connectionIP = GUILayout.TextField (connectionIP);
+			//portNumber = int.Parse (GUILayout.TextField (portNumber.ToString ));
+			if (GUILayout.Button ("Connect"))
+				Network.Connect (connectionIP, portNumber);
+			if (GUILayout.Button ("Host"))
+					Network.InitializeServer (4, portNumber, false);
+		}
+		else
+			GUILayout.Label ("Connections: " + Network.connections.Length.ToString ());
+
+	}
+}
