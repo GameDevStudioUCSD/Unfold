949a1a135ba16e515d1a106549397b0dbc2b657e
Eric Cheng <ericcheng95@gmail.com>
2014-10-24 09:13:41 -0700

    Added Player Prefab
    Added Player Prefab into Prefab folder.

---
+++ ProjectLabyrinth/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float xAxis, zAxis = 0.0f;
+	
+	float xAngle, zAngle = 0.0f;
+	float zVelocity = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xAxis = Input.GetAxis("Horizontal");
+		zAxis = Input.GetAxis("Vertical");
+		
+		transform.Translate (xAxis, 0, zAxis);
+		
+		//transform.Rotate(xAngle, 0, zAngle);
+		//transform.Translate(0, 0, zVelocity);
+	}
+}
