b7eef005b4eccf008dbc5bb12cd5b6ad2659c489
Eric Cheng <ericcheng95@gmail.com>
2014-10-31 01:38:03 -0700

    Collider and Trigger working
    Collider Capsule not moving with Controller

---
+++ ProjectLabyrinth/Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+	
+	void OnCollisionEnter (Collision other)
+	{
+		Debug.Log ("Object Entered the Collision");
+	}
+	
+	/*void OnCollisionStay (Collision other)
+	{
+		Debug.Log ("Object Inside the trigger");
+	}
+	
+	void OnCollisionExit (Collision other)
+	{
+		Debug.Log ("Object has Exited the trigger");
+	}*/
+}
---
+++ ProjectLabyrinth/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerTest : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+	
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log(other.name + " Entered");
+	}
+	
+	void OnTriggerStay (Collider other)
+	{
+		Debug.Log(other.name + " Staying");
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log(other.name + " Exited");
+	}
+}
