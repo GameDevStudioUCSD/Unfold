809934b0a7642b3c21e74e00b2d822367e2590fd
MichaelMGonzalez <mmg005@ucsd.edu>
2014-10-31 22:10:57 -0700

    Added script outlines for maze generation and each cell representation of each square

---
+++ ProjectLabyrinth/Assets/Scripts/Maze_Generator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze_Generator : MonoBehaviour {
+    public int Rows = 10;
+    public int Cols = 10;
+    public GameObject WallStub;
+    private bool[,] walls;
+	// Use this for initialization
+	void Start () {
+        walls = new bool[Rows,Cols];
+        for (int r = 0; r < Rows; r++)
+        {
+            for (int c = 0; c < Cols; c++)
+            {
+            }
+        }
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
---
+++ ProjectLabyrinth/Assets/Scripts/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+public class Square
+{
+    public bool visited {get; set;}
+    public bool hasNorth {get; set;}
+    public bool hasSouth {get; set;}
+    public bool hasWest {get; set;}
+    public bool hasEast { get; set; }
+    public int row { get; }
+    public int col { get; }
+	public Square(int r, int c)
+	{
+        row = r;
+        col = c;
+        hasNorth = true;
+        hasSouth = true;
+        hasWest = true;
+        hasEast = true;
+	}
+}
