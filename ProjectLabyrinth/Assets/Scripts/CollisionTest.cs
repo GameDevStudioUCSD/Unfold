021e1b6a1c5eabc332c5dda0ae1064c7006a9c7b
Eric Cheng <ericcheng95@gmail.com>
2014-11-02 16:13:52 -0800

    Trigger Update
    Newest Script

--- ProjectLabyrinth/Assets/Scripts/CollisionTest.cs
+++ ProjectLabyrinth/Assets/Scripts/CollisionTest.cs
@@ -2,16 +2,6 @@
 using System.Collections;
 
 public class CollisionTest : MonoBehaviour {
-
-	// Use this for initialization
-	/*void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
 	
 	void OnCollisionEnter (Collision other)
 	{
