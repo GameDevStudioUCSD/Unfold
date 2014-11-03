3b35bffc13f1858f83d073238f141f76788d86e2
Eric Cheng <ericcheng95@gmail.com>
2014-11-02 17:16:15 -0800

    Attack Trigger detecting AttackVictim
    Left Click to trigger attack. If there is an object within the attack
    collider, it will register the object and print it in the console

---
+++ ProjectLabyrinth/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	Collider attackVictim;
+	
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log ("MOUSE PRESSED");
+			if (attackVictim)
+				Debug.Log ("Attacking " + attackVictim.name);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		attackVictim = other;
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		attackVictim = null;
+	}
+	
+}
