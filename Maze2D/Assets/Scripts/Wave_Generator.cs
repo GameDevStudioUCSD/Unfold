9decdfd7e3e1f56e13c9522a0954f4166056b98e
MichaelMGonzalez <mmg005@ucsd.edu>
2014-11-10 13:01:14 -0800

    Made inconsequentail changes to wave generator example (Necissary commit to merge)

--- Maze2D/Assets/Scripts/Wave_Generator.cs
+++ Maze2D/Assets/Scripts/Wave_Generator.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Wave_Generator : MonoBehaviour {
     public GameObject wave;
+    public int size = 20;
 	// Use this for initialization
 	void Start () {
-        for (int z = 0; z < 100; z++)
+        for (int z = 0; z < size; z++)
         {
-            for (int x = 0; x < 100; x++)
+            for (int x = 0; x < size; x++)
             {
 
                 Instantiate(wave, new Vector3(x, 
