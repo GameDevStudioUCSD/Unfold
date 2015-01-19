f17014c180ea76441acea0550526d089f22903b6
Eric Cheng <ericcheng95@gmail.com>
2015-01-19 00:27:44 -0800

    Game now starts only after host clicks "Start Game"

--- ProjectLabyrinth/Assets/Scripts/Network/NetworkManager.cs
+++ ProjectLabyrinth/Assets/Scripts/Network/NetworkManager.cs
@@ -6,12 +6,18 @@ public class NetworkManager : MonoBehaviour {
 	public bool debug_On;
 	public GameObject playerPrefab;
 	public GameObject mazeGenerator;
+	public NetworkView nView;
 
 	private MazeGeneratorController mapCreator;
+	private bool serverStarted;
+	private bool matchStarted;
+	private bool gameList;
 
 	private const string typeName = "UniqueGameName";
 	private const string gameName = "RoomName";
 	private HostData[] hostList;
+	private NetworkPlayer[] playerList;
+	private int playerCount;
 	
 	//MasterServer.ipAddress = "127.0.0.1";
 	
@@ -19,38 +25,15 @@ public class NetworkManager : MonoBehaviour {
 	{
 		Network.InitializeServer (4, 25000, !Network.HavePublicAddress());
 		MasterServer.RegisterHost(typeName, gameName);
-		mapCreator = (MazeGeneratorController)mazeGenerator.GetComponent(typeof(MazeGeneratorController));
-        //mapCreator = new MazeGeneratorController(MazeGeneratorController.DepthFirst);
+		/*mapCreator = (MazeGeneratorController)mazeGenerator.GetComponent(typeof(MazeGeneratorController));
 		mapCreator.Start();
-		mapCreator.createWalls();
-        //Debug.Log(mapCreator.getStartSquare());
-		SpawnPlayer();
-	}
-	
-	void OnServerInitialized()
-	{
-		if (debug_On)
-			Debug.Log ("Server Initialized");
+		mapCreator.createWalls();*/
+		if(Network.isServer)
+			nView.RPC("updatePlayerList",RPCMode.All);
+		
+		serverStarted = true;
+        
 		//SpawnPlayer();
-	}
-	
-	void OnGUI()
-	{
-		if (!Network.isClient && !Network.isServer)
-		{
-			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-				StartServer();
-			if (GUI.Button(new Rect(100, 300, 250, 100), "Refresh Hosts"))
-				RefreshHostList();
-			if (hostList != null)
-			{
-				for (int i = 0; i < hostList.Length; i++)
-				{
-					if (GUI.Button(new Rect(400, 100+(110*i), 300, 100), hostList[i].gameName))
-						JoinServer(hostList[i]);
-				}
-			}	
-		}
 	}
 	
 	private void RefreshHostList()
@@ -61,7 +44,9 @@ public class NetworkManager : MonoBehaviour {
 	void OnMasterServerEvent(MasterServerEvent msEvent)
 	{
 		if (msEvent == MasterServerEvent.HostListReceived)
-			hostList = MasterServer.PollHostList();		
+			hostList = MasterServer.PollHostList();
+			
+		
 	}
 	
 	private void JoinServer(HostData hostData)
@@ -73,12 +58,82 @@ public class NetworkManager : MonoBehaviour {
 	{
 		if (debug_On)
 			Debug.Log ("Server Joined");
-		SpawnPlayer();
+		if(Network.isClient)
+			nView.RPC("updatePlayerList",RPCMode.All);
+	}
+	
+	void OnServerInitialized()
+	{
+		if (debug_On)
+			Debug.Log ("Server Initialized");
+		//SpawnPlayer();
+	}
+	
+	void OnGUI()
+	{
+		if (!matchStarted/*!Network.isClient && !Network.isServer*/)
+		{
+			if (!serverStarted && !gameList)
+			{
+				if (GUI.Button(new Rect(100, 100, 250, 100), "Create Game"))
+					StartServer();
+				if (GUI.Button(new Rect(100, 300, 250, 100), "Join Game"))
+				{
+					RefreshHostList();
+					gameList = true;
+				}
+			}
+			if (hostList != null && gameList == true)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(100, 100+(110*i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+				if (GUI.Button (new Rect(100, 500, 250, 100), "Back"))
+					gameList = false;
+			}
+			
+			if (serverStarted)
+			{
+				if (GUI.Button(new Rect(100, 500, 250, 100), "Start Game"))
+					StartMatch ();
+				
+			}
+		}
+	}
+	
+	[RPC]
+	private void updatePlayerList()
+	{
+		//playerList[playerCount] = Network.player;
+		playerCount++;
+		Debug.Log("Player Count: " + playerCount);
+	}
+	
+	[RPC]
+	private void updateMatchStatus(bool matchStatus)
+	{
+		bool prevStatus = matchStarted;
+		matchStarted = matchStatus;
+		if (prevStatus == false && matchStarted == true && Network.isClient)
+			SpawnPlayer();
+	}
+	
+	private void StartMatch()
+	{
+		mapCreator = (MazeGeneratorController)mazeGenerator.GetComponent(typeof(MazeGeneratorController));
+		mapCreator.Start();
+		mapCreator.createWalls();
+		//updateMatchStatus(true);
+		if(Network.isServer)
+			nView.RPC("updateMatchStatus",RPCMode.All, true);
+		SpawnPlayer ();
+		
 	}
 	
 	private void SpawnPlayer()
 	{
-		//mapCreator.createPlayer();
 		Network.Instantiate (playerPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
 	}
 	
