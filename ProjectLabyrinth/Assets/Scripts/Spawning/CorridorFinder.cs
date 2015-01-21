17751ab0cc5edd7b0112bdb37e41796018a674bf
MichaelMGonzalez <mmg005@ucsd.edu>
2015-01-20 17:52:29 -0800

    Filled out method body for FindCorridors. Created method stub for GetAdjacentWalls

--- ProjectLabyrinth/Assets/Scripts/Spawning/CorridorFinder.cs
+++ ProjectLabyrinth/Assets/Scripts/Spawning/CorridorFinder.cs
@@ -6,9 +6,21 @@ public class CorridorFinder : MonoBehaviour
 	// Walks through a maze and builds a list of Squares and their
 	// corridor's length. The length of each corridor will be used
 	// as a weight for spawning monsters.
-	public static ArrayList FindCorridors (Square[,] maze)
+	public static ArrayList FindCorridors (Square[,] maze, int rows, int cols)
 	{
-		return null;
+        ArrayList listOfCorridors = new ArrayList();
+        for (int r = 0; r < rows; r++ )
+        {
+            for( int c = 0; c < cols; c++)
+            {
+                if( GetAdjacentWalls(maze[r,c]) == 3)
+                {
+                    listOfCorridors.Add(maze[r, c]);
+                    CorridorWalker(maze[r, c]);
+                }
+            }
+        }
+            return listOfCorridors;
 	}
 
 	// Walks backwards from the end of a corridor (Square with 3 walls
@@ -17,4 +29,9 @@ public class CorridorFinder : MonoBehaviour
 	{
 		return 0;
 	}
+
+    private static int GetAdjacentWalls(Square cell)
+    {
+        return 0;
+    }
 }
