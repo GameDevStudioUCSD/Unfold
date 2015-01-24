c34237fca75ba7667773564e0e6846235b8ba959
Alec Brickner <alecjbrick@hotmail.com>
2015-01-23 19:03:57 -0800

    Monster can move back and forth

--- ProjectLabyrinth/Assets/Scripts/TestScripts/MonsterMovement.cs
+++ ProjectLabyrinth/Assets/Scripts/TestScripts/MonsterMovement.cs
@@ -3,25 +3,28 @@ using System.Collections;
 
 public class MonsterMovement : MonoBehaviour {
 
-	private MazeGeneratorController mazeGen;
+	public MazeGeneratorController mazeGen;
 	private Square[,] walls;
 	float dir = .1f;
-	bool begin = true;
+	bool canTurn = false;
 
 	// Use this for initialization
 	void Start () {
-		mazeGen = GameObject.Find ("Maze Generator").GetComponent<MazeGeneratorController>();
 		walls = mazeGen.getWalls ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (dir, 0, 0);
-		if (!begin && transform.position.x % 10 == 0) {
-						dir *= -1;
-		} else if (transform.position.x % 10 == 0) {
-			begin = false;
+		if (Mathf.Abs (transform.position.x - Mathf.Round (transform.position.x)) < .2 &&
+			Mathf.Round (transform.position.x) % 10 == 0 && canTurn) {
+
 			dir *= -1;
+		}
+
+		else if (Mathf.Abs (transform.position.x - Mathf.Round (transform.position.x)) < .2 &&
+		         Mathf.Round (transform.position.x) % 10 == 5){
+			canTurn = true;
 		}
 	}
 
