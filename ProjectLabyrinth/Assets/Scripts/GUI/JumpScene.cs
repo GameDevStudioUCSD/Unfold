79259a40f3a4d6d90e483343e58c9cf5425b2b25
Eric Cheng <ericcheng95@gmail.com>
2015-02-07 16:27:46 -0800

    Added main menu hallway functionality

---
+++ ProjectLabyrinth/Assets/Scripts/GUI/JumpScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScene : MonoBehaviour {
+
+	public GameObject loadedScene;
+	public bool debug_On;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Collider attackCollider = other;
+		GameObject attackObject = attackCollider.gameObject;
+		if (debug_On)
+			Debug.Log ("Collider set to " + attackCollider.name);
+			
+		Instantiate(loadedScene, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+}
