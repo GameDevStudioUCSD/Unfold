1945b9534bf4beab399f225725bef2b1d94127ce
withoutaname <drevitchi@gmail.com>
2015-02-10 06:27:30 -0800

    Consolidated PlayerAttack and MonsterAttack into Character classes Commented out Mortality.cs as it was preventing the game from being played

--- ProjectLabyrinth/Assets/Scripts/Character/EnemyCharacter.cs
+++ ProjectLabyrinth/Assets/Scripts/Character/EnemyCharacter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 /**
@@ -8,9 +9,44 @@ using UnityEngine;
  */
 public class EnemyCharacter : Character {
 
+	/**
+	 * Determines what kind of weakness this enemy has, if any
+	 *
+	 * 0000 = no weaknesses
+	 * 0001 = horizontal weakness
+	 * 0010 = vertical weakness
+	 * 0100 = diagonal1 weakness (topright -> bottomleft)
+	 * 1000 = diagonal2 weakness (topleft -> bottomright)
+	 */
+	public int weakness;
+
 	void Start() {
 	}
 
-	void Update() {
+	void FixedUpdate() {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		this.attackCollider = other;
+	}
+
+	void OnTriggerExit(Collider other) {
+		this.attackCollider = null;
+	}
+
+	public override void TakeDamage(int enDamage, int enAttackType) {
+		if (enAttackType == this.weakness) {
+			enDamage = enDamage * 2;
+		}
+
+		this.health = this.health - enDamage;
+		if (this.health <= 0) {
+			this.Die();
+		}
+		Debug.Log("Damage: " + enDamage);
+	}
+
+	public override void Die() {
+		Destroy(this.gameObject);
 	}
 }
