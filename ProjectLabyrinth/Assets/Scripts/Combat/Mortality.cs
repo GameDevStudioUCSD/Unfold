523373e7f3afd6bb47315d30cc7e962f3fbeb2f5
Alec Brickner <alecjbrick@hotmail.com>
2015-02-13 19:23:11 -0800

    Added Mortality

--- ProjectLabyrinth/Assets/Scripts/Combat/Mortality.cs
+++ ProjectLabyrinth/Assets/Scripts/Combat/Mortality.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class Mortality : IFightable {
-	override public void Die()
+	/*override public void Die()
 	{
 		//TODO Write up a lose condition and terminate player.
 		Debug.Log("YOU ARE DEAD!");
-	}
+	}*/
 
 }
