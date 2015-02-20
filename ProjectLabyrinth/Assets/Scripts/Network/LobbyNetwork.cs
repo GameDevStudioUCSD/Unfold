f615a78c12ae2c78165973076599b4f8a507d85a
ssl024 <ssl024@ucsd.edu>
2015-02-19 21:36:40 -0800

    Network Integration
    Attempting to integrate network - Steven

---
+++ ProjectLabyrinth/Assets/Scripts/Network/LobbyNetwork.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyNetwork : MonoBehaviour {
+
+    public GameObject playerPrefab;
+    public GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        SpawnPlayer();
+	}
+
+    private void SpawnPlayer()
+    {
+        Network.Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity, 0);
+    }
+}
---
+++ ProjectLabyrinth/Assets/Scripts/Network/NetworkLeaveGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkLeaveGame : MonoBehaviour {
+
+    public string nextScene = "MultiplayerMenu";
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if(Network.isClient)
+        {
+            Network.CloseConnection(Network.connections[0], true);
+        }
+
+        if(Network.isServer)
+        {
+            Network.Disconnect();
+        }
+    }
+
+    void OnDisconnectedFromServer()
+    {
+        Application.LoadLevel(nextScene);
+    }
+}
