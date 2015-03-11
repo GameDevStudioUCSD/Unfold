cdcf75d57f34cdf604459c43be2ce3468599547e
withoutaname <drevitchi@gmail.com>
2015-03-10 20:38:54 -0700

    Moved health bar down into the viewable canvas Provided comments/documentation for ResultsScreen.cs

--- ProjectLabyrinth/Assets/Scripts/ResultsScreen.cs
+++ ProjectLabyrinth/Assets/Scripts/ResultsScreen.cs
@@ -7,16 +7,28 @@ using UnityEngine.UI;
 /// </summary>
 public class ResultsScreen : MonoBehaviour {
 
+	/// <summary>
+	/// TextUI displaying total amount of time spent on this game.
+	/// </summary>
 	public Text timeWasted;
 
+	/// <summary>
+	/// The player viewing this results screen.
+	/// </summary>
 	public PlayerCharacter player;
+
+	/// <summary>
+	/// TextUI elements displaying player statistics.
+	/// </summary>
 	public Text playerMaxHealth;
 	public Text playerDamage;
 
 	void Start() {
+		// Format the time in seconds into an appropriate string
 		TimeSpan timeSpan = TimeSpan.FromSeconds ((int)Time.time);
 		string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}",
 		                                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
 		this.timeWasted.text = "Time wasted on this game: " + timeText;
 
 		if (this.player) {
