6210c0973985c0f160ae3d00aa6fcc20ade29070
ssl024 <ssl024@ucsd.edu>
2015-03-27 17:20:23 -0700

    Player Prefab HUD changes for networking
    Made some changes to the player prefab so it can work with network.
    Changed the GameHUD script to include a networkView.ismine check.

--- ProjectLabyrinth/Assets/Scripts/GameHUD.cs
+++ ProjectLabyrinth/Assets/Scripts/GameHUD.cs
@@ -5,67 +5,85 @@ using UnityEngine;
 /// Class to control the behavior of all aspects of the player's interface
 /// whilst in an instance of the game scene.
 /// </summary>
-public class GameHUD : MonoBehaviour {
+public class GameHUD : MonoBehaviour
+{
 
-	/// <summary>
-	/// The actual player character.
-	/// </summary>
-	public PlayerCharacter player;
+    /// <summary>
+    /// NetworkView required to check if player is mine.
+    /// </summary>
+    public NetworkView networkView;
 
-	/// <summary>
-	/// Gets or sets the user interface camera.
-	/// </summary>
-	/// <value>The user interface camera.</value>
-	public Camera UICamera { get; set; }
+    /// <summary>
+    /// The actual player character.
+    /// </summary>
+    public PlayerCharacter player;
 
-	/// <summary>
-	/// The joystick used for determining user input.
-	/// </summary>
-	public CustomJoystick Joystick;
+    /// <summary>
+    /// Gets or sets the user interface camera.
+    /// </summary>
+    /// <value>The user interface camera.</value>
+    public Camera UICamera { get; set; }
 
-	/// <summary>
-	/// The health system for the player.
-	/// </summary>
-	public HealthSystem healthSystem;
+    /// <summary>
+    /// The joystick used for determining user input.
+    /// </summary>
+    public CustomJoystick Joystick;
 
-	/// <summary>
-	/// Gets or sets the particle generator.
-	/// </summary>
-	/// <value>The particle generator.</value>
-	public ParticleGenerator particles { get; set; }
+    /// <summary>
+    /// The health system for the player.
+    /// </summary>
+    public HealthSystem healthSystem;
 
-	/// <summary>
-	/// Object initialization method.
-	/// </summary>
-	void OnEnable() {
-		this.UICamera = this.GetComponent<Canvas>().worldCamera;
-		if (this.player == null) {
-			this.player = this.transform.root.GetComponentInChildren<PlayerCharacter>();
-		}
-		if (this.Joystick != null) {
-			this.Joystick.ControllerMovedEvent += this.player.Move;
-			this.Joystick.FingerLiftedEvent += this.player.Idle;
-		}
-		this.particles = this.GetComponent<ParticleGenerator>();
-	}
+    /// <summary>
+    /// Gets or sets the particle generator.
+    /// </summary>
+    /// <value>The particle generator.</value>
+    public ParticleGenerator particles { get; set; }
 
-	// Update is called once per frame
-	void Update() {
-		if (this.Joystick != null) {
-			this.Joystick.handleInput(this.UICamera);
-		}
-		if (this.healthSystem != null) {
-			this.healthSystem.display(this.player);
-		}
-		foreach (Touch currentTouch in Input.touches) {
-			if (currentTouch.phase == TouchPhase.Began) {
-				Vector3 worldPosition = this.UICamera.ScreenToWorldPoint(currentTouch.position);
+    /// <summary>
+    /// Object initialization method.
+    /// </summary>
+    void OnEnable()
+    {
+        this.UICamera = this.GetComponent<Canvas>().worldCamera;
+        if (this.player == null)
+        {
+            this.player = this.transform.root.GetComponentInChildren<PlayerCharacter>();
+        }
+        if (this.Joystick != null)
+        {
+            this.Joystick.ControllerMovedEvent += this.player.Move;
+            this.Joystick.FingerLiftedEvent += this.player.Idle;
+        }
+        this.particles = this.GetComponent<ParticleGenerator>();
+    }
 
-				// TODO: Add pause button functionality
-				if (!this.Joystick.IsStickActive()) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (networkView.isMine)
+        {
+            if (this.Joystick != null)
+            {
+                this.Joystick.handleInput(this.UICamera);
+            }
+            if (this.healthSystem != null)
+            {
+                this.healthSystem.display(this.player);
+            }
+            foreach (Touch currentTouch in Input.touches)
+            {
+                if (currentTouch.phase == TouchPhase.Began)
+                {
+                    Vector3 worldPosition = this.UICamera.ScreenToWorldPoint(currentTouch.position);
 
-				}
-			}
-		}
-	}
+                    // TODO: Add pause button functionality
+                    if (!this.Joystick.IsStickActive())
+                    {
+
+                    }
+                }
+            }
+        }
+    }
 }
