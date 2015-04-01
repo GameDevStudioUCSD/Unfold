c27dc663bb9098b620a68c06f54aebd9a5bfef7c
AnhQuan Nguyen <j4qfrost@gmail.com>
2015-04-01 00:46:09 -0700

    Dummy Results

---
+++ ProjectLabyrinth/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	public const int NUMBER_OF_PLAYERS = 6;
+	public PlayerData[] stats = new PlayerData[NUMBER_OF_PLAYERS];
+
+}
