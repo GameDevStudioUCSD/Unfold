a51be4e96176df9ff0d2f8814248280c5e57b337
withoutaname <drevitchi@gmail.com>
2015-04-12 17:28:19 -0700

    Scaled back camera values so player model doesn't take up 50% of the screen

--- ProjectLabyrinth/Assets/Scripts/Maze/ExitCollision.cs
+++ ProjectLabyrinth/Assets/Scripts/Maze/ExitCollision.cs
@@ -20,10 +20,7 @@ public class ExitCollision : AbstractGUI {
 			this.player = (PlayerCharacter) hitDetector.GetComponentInParent<PlayerCharacter>();
 			this.hasReached = true;
 			skPrefab.GetComponent<ScoreKeeper>().stats[0].win = true;
-<<<<<<< HEAD
             Instantiate(loadResult, new Vector3(0, 0, 0), Quaternion.identity);
-=======
->>>>>>> bc3897313e3ea4ac4a946f5481c7226f4f28fe39
 			player.data.win = true;
 		}
 	}
