1b22f17a659328ecaf37ff9db3c89c156f067617
Alec Brickner <alecjbrick@hotmail.com>
2015-04-16 21:06:03 -0700

    Added the Maze Info object

--- ProjectLabyrinth/Assets/Scripts/Maze/MazeInfo.cs
+++ ProjectLabyrinth/Assets/Scripts/Maze/MazeInfo.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class MazeInfo : MonoBehaviour {
 	public MazeGeneratorController maze;
 	Square[,] walls;
+	float wallSize;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		wallSize = maze.wallSize;
 	}
 	
 	// Update is called once per frame
@@ -16,7 +17,17 @@ public class MazeInfo : MonoBehaviour {
 	
 	}
 
-	Square[,] getWalls() {
+	public Square[,] getWalls() {
 		return this.walls;
+	}
+
+	public Square getCurrSquare(float x, float z) {
+		int initRow = (int) Mathf.Round (x / wallSize);
+		int initCol = (int) Mathf.Round (z / wallSize);
+		return walls [initRow, initCol];
+	}
+
+	public float getWallSize() {
+		return wallSize;
 	}
 }
