7175104072449eb379a1e4a2809cc54eba65e5e2
raghav96 <raghav96@gmail.com>
2015-04-16 22:22:26 -0700

    MenuWalk
    Walks the player into a lecturn(to be implemented). Still need to
    implement going into portal afterwards.

---
+++ ProjectLabyrinth/Assets/Scripts/MenuWalk/MenuWalk.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuWalk : MonoBehaviour {
+	public Transform startMarker;
+	public Transform endMarker;
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+	}
+
+}
