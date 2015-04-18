54e8d00f43463c19db7bd71855abff43e0db9114
Michael Gonzalez <mmg005@ucsd.edu>
2015-04-17 20:25:13 -0700

    Made it so player will trigger monolith UI

---
+++ ProjectLabyrinth/Assets/MonolithControllerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonolithControllerScript : MonoBehaviour {
+
+    public Canvas canvasUIPrefab;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Monolith Triggered!");
+
+        Instantiate(canvasUIPrefab);
+
+    }
+}
