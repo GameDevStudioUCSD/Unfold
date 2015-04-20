5bbf6885e0765bf4ab4692c01e0bebdb4f2e9beb
Alec Brickner <alecjbrick@hotmail.com>
2015-04-20 16:21:55 -0700

    For Alexander Kafka (or anyone) to look at

--- ProjectLabyrinth/Assets/Scripts/Maze/FogOfWar.cs
+++ ProjectLabyrinth/Assets/Scripts/Maze/FogOfWar.cs
@@ -23,12 +23,12 @@ public class FogOfWar : MonoBehaviour {
         }
         int initRow = (int)Mathf.Round(player.transform.position.x / mi.getWallSize());
         int initCol = (int)Mathf.Round(player.transform.position.z / mi.getWallSize());
-        Square currWalls = mi.getWalls()[initRow, initCol];
+/*        Square currWalls = mi.getWalls()[initRow, initCol];
         if (!currWalls.playerVisited)
         {
             Light.Instantiate(this.playerHasSeen, new Vector3(initRow, 5, initCol), Quaternion.identity);
             currWalls.playerVisited = true;
-        }
+        }*/
         
 		
 	}
--- ProjectLabyrinth/Assets/Scripts/Maze/MazeInfo.cs
+++ ProjectLabyrinth/Assets/Scripts/Maze/MazeInfo.cs
@@ -5,16 +5,22 @@ public class MazeInfo : MonoBehaviour {
 	public MazeGeneratorController maze;
 	Square[,] walls;
 	float wallSize;
+	public bool debug;
 
 
 	// Use this for initialization
 	void Start () {
-		wallSize = maze.wallSize;
+		if (debug)
+			Debug.Log ("I was called!");
+
+		this.wallSize = maze.wallSize;
+		this.walls = maze.getWalls ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (debug)
+			Debug.Log ("I was called too!");
 	}
 
 	public Square[,] getWalls() {
@@ -29,5 +35,36 @@ public class MazeInfo : MonoBehaviour {
 
 	public float getWallSize() {
 		return wallSize;
+	}
+
+	// In order: south, west, north, east.
+	public bool[] getSquareWalls(Square s) {
+		bool south, west, north, east;
+
+		int x = s.getRow ();
+		int z = s.getCol ();
+		// Because some mazes don't generate a wall on both sides of the wall, we need to
+		// check the next square over as well.
+		if ((x + 1) < maze.Rows)
+			south = walls[x + 1, z].hasNorth;
+		else
+			south = true;
+		
+		if ((x - 1) >= 0)
+			north = walls[x - 1, z].hasSouth;
+		else
+			north = true;
+		
+		if ((z - 1) >= 0)
+			west = walls[x, z - 1].hasEast;
+		else
+			west = true;
+		
+		if ((z + 1) < maze.Cols)
+			east = walls[x, z + 1].hasWest;
+		else
+			east = true;
+		
+		return new[] {s.hasSouth || south, s.hasWest || west, s.hasNorth || north, s.hasEast || east};
 	}
 }
