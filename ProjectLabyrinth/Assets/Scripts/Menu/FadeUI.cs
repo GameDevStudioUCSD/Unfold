441ba8074ac05023824272fb218c9b55f9f45008
MichaelMGonzalez <mmg005@ucsd.edu>
2015-04-26 13:10:47 -0700

    Menu UI Updates
    Created error messages on create menu for the following cases:
    - No Game Name
    - Not connected to internet
    - Could not connect to master server
    Created fade UI script to fade error messages and title screen
    Fixed Game Name Spacing issue
    Changed back buttons to red sticky notes

---
+++ ProjectLabyrinth/Assets/Scripts/Menu/FadeUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class FadeUI : MonoBehaviour {
+    public Graphic[] elementsToFade;
+    public float lengthOfFade = 2;
+    public float lengthBeforeFade = 2;
+    private Color[] originalColors;
+    private Color[] endColors;
+    private float startTime ;
+	void Start () {
+        originalColors = new Color[elementsToFade.Length];
+        endColors = new Color[elementsToFade.Length];
+        int i = 0;
+        float r, g, b;
+        // Save a copy of each original color
+        foreach (Graphic elem in elementsToFade)
+        {
+            r = elem.color.r;
+            g = elem.color.g;
+            b = elem.color.b;
+            originalColors[i] = new Color(r, g, b, 1);
+            endColors[i] = new Color(r, g, b, 0);
+            i++;
+        }
+        // Set the start and end times
+        startTime = Time.time + lengthBeforeFade;
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Time.time < startTime)
+            return;
+        float percentToLerp = (Time.time - startTime)/lengthOfFade;
+        int i = 0;
+        foreach(Graphic elem in elementsToFade)
+        {
+            elem.color = Color.Lerp(originalColors[i], endColors[i], percentToLerp);
+            i++;
+        }
+        if (Time.time - startTime > lengthOfFade)
+            Destroy(this.GetComponent<Transform>().gameObject);
+	}
+}
