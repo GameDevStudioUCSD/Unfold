ccfebcbf404297a11c22c8333dcb21c433281762
Michael Gonzalez <mmg005@ucsd.edu>
2015-05-02 22:35:53 -0700

    Network framework for rotations and animations

--- ProjectLabyrinth/Assets/Scripts/Network/NetworkCharacter.cs
+++ ProjectLabyrinth/Assets/Scripts/Network/NetworkCharacter.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class NetworkCharacter : MonoBehaviour {
     Vector3 truePosition;
+    Quaternion trueRotation;
+    int animationState;
     RectTransform trans;
     NetworkView nView;
     Animator animator;
 	// Use this for initialization
+    public enum PlayerAnimation 
+    {
+        Idle,
+        Walking 
+    }
 	void Start () {
         nView = this.GetComponent<NetworkView>();
         trans = this.GetComponent<RectTransform>();
@@ -19,12 +26,20 @@ public class NetworkCharacter : MonoBehaviour {
         if(stream.isWriting)
         { //Sending
             truePosition = trans.position;
+            trueRotation = trans.rotation;
+            animationState = (int)DetermineAnimationState();
             stream.Serialize(ref truePosition);
+            stream.Serialize(ref trueRotation);
+            stream.Serialize(ref animationState);
         }
         else
         { // Receiving
             stream.Serialize(ref truePosition);
+            stream.Serialize(ref trueRotation);
+            stream.Serialize(ref animationState);
             UpdatePosition(trans.position, truePosition);
+            UpdateRotation(trans.rotation, trueRotation);
+            UpdateAnimationState((PlayerAnimation)animationState);
         }
 	}
     void UpdatePosition(Vector3 a, Vector3 b)
@@ -32,5 +47,32 @@ public class NetworkCharacter : MonoBehaviour {
         if (Vector3.Distance(a, b) < .5f)
             return;
         trans.position = Vector3.Lerp(a, b, Time.deltaTime * 5);
+    }
+    void UpdateRotation(Quaternion a, Quaternion b)
+    {
+        if (Quaternion.Angle(a, b) < 10)
+            return;
+        trans.rotation = Quaternion.Slerp(a, b, Time.deltaTime * 5);
+    }
+    void UpdateAnimationState(PlayerAnimation animation)
+    {
+        switch (animation)
+        {
+            case PlayerAnimation.Idle:
+                animator.SetBool("Walking", false);
+                break;
+            case PlayerAnimation.Walking:
+                animator.SetBool("Walking", true);
+                break;
+        }
+    }
+    PlayerAnimation DetermineAnimationState()
+    {
+        PlayerAnimation retVal = PlayerAnimation.Idle;
+        if(animator.GetBool("Walking"))
+        {
+            retVal = PlayerAnimation.Walking;
+        }
+        return retVal;
     }
 }
