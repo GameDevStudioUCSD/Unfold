2af7bfc124b62b6ef9ff4c7be3617c03b7b62144
Eric Cheng <ericcheng95@gmail.com>
2015-05-17 16:17:33 -0700

    added textures and rebalanced the menu walls

--- ProjectLabyrinth/Assets/Scripts/Textures/TextureDB.cs
+++ ProjectLabyrinth/Assets/Scripts/Textures/TextureDB.cs
@@ -12,12 +12,13 @@ public class TextureDB : MonoBehaviour {
         db.Add("cave_ceiling", Resources.Load("Textures/cave_ceiling", typeOfDB) as Texture);
         db.Add("cave_wall_1", Resources.Load("Textures/cave_wall_1", typeOfDB) as Texture);
         db.Add("forest_floor", Resources.Load("Textures/forest_floor", typeOfDB) as Texture);
-        db.Add("forest_ceiling", Resources.Load("Textures/forest_ceiling", typeOfDB) as Texture);
+        db.Add("forest_ceiling", Resources.Load("Textures/Forest_canopy_v2_merged", typeOfDB) as Texture);
         db.Add("forest_wall_1", Resources.Load("Textures/forest_wall_1", typeOfDB) as Texture);
         db.Add("corn_wall_1", Resources.Load("Textures/corn_wall_1", typeOfDB) as Texture);
         db.Add("corn_wall_2", Resources.Load("Textures/corn_wall_2", typeOfDB) as Texture);
         db.Add("corn_wall_3", Resources.Load("Textures/corn_wall_3", typeOfDB) as Texture);
-        db.Add("corn_floor", Resources.Load("Textures/corn_floor", typeOfDB) as Texture);
+        db.Add("corn_floor", Resources.Load("Textures/GroundPlaceholder", typeOfDB) as Texture);
+        db.Add("corn_ceiling", Resources.Load ("Textures/CORN_SKYv3", typeOfDB) as Texture);
         return db;
     }
     public static int NumberOfWallTextures(TextureController.TextureChoice texture)
