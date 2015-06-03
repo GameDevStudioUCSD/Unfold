2b2afda26b97d51090c9a4174c599b0e649ac2a7
Michael Gonzalez <mmg005@ucsd.edu>
2015-06-03 02:00:44 -0700

    Created oscillation dector
    The detector catches mishaps in player y variable oscillations
    This also lays the framework to detect monster oscillations

--- Unfold/Assets/Scripts/Network/NetworkUser.cs
+++ Unfold/Assets/Scripts/Network/NetworkUser.cs
@@ -18,7 +18,26 @@ public class NetworkUser : NetworkCharacter{
     }
     protected override void NormalizePosition()
     {
-        if (Network.isClient)
+        // Detect oscillations
+        // If the actual y value isn't equal to the current y value,
+        // then there is a strong chance there might be an oscillation
+        if(trans.position.y != lastY || differenceCounter > DIFFERENCETHRESHOLD)
+        {
+            // Increment difference counter
+            differenceCounter++;
+            // Update lastY
+            lastY = trans.position.y;
+        }
+        // Otherwise, there probably isn't any oscillations
+        else
+        {
+            // Reset difference counter
+            differenceCounter = 0;
+        }
+        // Only normalize the vector if the on a client machine and difference
+        // counter does not exceed the threshold
+        Debug.Log(differenceCounter);
+        if (Network.isClient && differenceCounter < DIFFERENCETHRESHOLD)
             truePosition += (1.5f * Vector3.up);
 
     }
