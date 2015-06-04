18094fb8e8e72e06425e9aad32e5e3c581f3174a
withoutaname <drevitchi@gmail.com>
2015-06-04 02:01:29 -0700

    Revert "Cave now has an exit"
    This reverts commit 2b5a526e2f527a6393ac8395ff5d9bf551f166c3.

--- Unfold/Assets/Scripts/Maze/RecursiveMaze.cs
+++ Unfold/Assets/Scripts/Maze/RecursiveMaze.cs
@@ -16,8 +16,6 @@ public class RecursiveMaze : MazeGenerator {
         createSquares(false);
         constructOutterWalls();
         generateMaze(0, 0, Rows , Cols);
-        base.selectEntrance();
-        EnsureExitExists();
     }
     private void constructOutterWalls()
     {
@@ -72,6 +70,7 @@ public class RecursiveMaze : MazeGenerator {
         }
         else if (!hasEntrance && r0 < (Rows * .2) && c0 < (Cols * .2))
         {
+            walls[r0, c0].start = true;
             hasEntrance = true;
         }
             
@@ -79,6 +78,7 @@ public class RecursiveMaze : MazeGenerator {
         {
             if (!hasExit  && r0 > (Rows*.8) && c0 > (Cols*.8))
             { 
+                walls[r0, c0].exit = true;
                 hasExit = true;
         
             }
