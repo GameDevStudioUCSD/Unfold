d04a77394c2a6ea2a8141fe727a4a391704b9e29
Eric Cheng <ericcheng95@gmail.com>
2015-06-04 01:59:04 -0700

    added you lose screen

--- Unfold/Assets/Scripts/Maze/ExitCollision.cs
+++ Unfold/Assets/Scripts/Maze/ExitCollision.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class ExitCollision : AbstractGUI {
 
-    public GameObject loadResult;
+    public GameObject victoryScreen;
+    public GameObject failureScreen1;
+    public GameObject failureScreen2;
+    
 	private PlayerCharacter player;
 	//public GameObject skPrefab;
 
@@ -18,23 +21,29 @@ public class ExitCollision : AbstractGUI {
         HitDetector hitDetector = gameObj.GetComponent<HitDetector>();
 		NetworkView nView = gameObj.GetComponentInParent<NetworkView>();
 		if (!hasGameEnded && hitDetector && nView && nView.isMine) {
-            Instantiate(loadResult, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(victoryScreen, new Vector3(0, 0, 0), Quaternion.identity);
             this.player = (PlayerCharacter)hitDetector.GetComponentInParent<PlayerCharacter>();
+            
 			//skPrefab.GetComponent<ScoreKeeper>().stats[0].win = true;
             //Instantiate(loadResult, new Vector3(0, 0, 0), Quaternion.identity);
+            
 			player.data.win = true;
 		}
         else if(!hasGameEnded && hitDetector)
         {
-            GameObject youLose = Instantiate(loadResult);
+            GameObject youLose = Instantiate(victoryScreen);
             foreach (Transform childTrans in youLose.transform)
             {
-                Text text = childTrans.gameObject.GetComponent<Text>();
-                if (text != null)
-                {
-                    text.fontSize = 32;
-                    text.text = "You lose...";
-                }
+            	System.Random rnd = new Random();
+            	int num = rnd.Next(0,2);
+				if (num)
+				{
+					Instantiate(failureScreen1, new Vector3(0, 0, 0), Quaternion.identity);
+				}
+				else
+				{
+					Instantiate(failureScreen2, new Vector3(0, 0, 0), Quaternion.identity);
+				}
             }
         }
 	}
