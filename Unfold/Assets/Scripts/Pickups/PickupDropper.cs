c27dbfa1c7e0963d79018b9497fd7df3266c154b
Alec Brickner <alecjbrick@hotmail.com>
2015-06-04 01:48:49 -0700

    More tutorial stuff

--- Unfold/Assets/Scripts/Pickups/PickupDropper.cs
+++ Unfold/Assets/Scripts/Pickups/PickupDropper.cs
@@ -8,6 +8,7 @@ public class PickupDropper: MonoBehaviour {
 	public int maxDrop;
 	public bool debug_On = false;
 	private System.Random rnd;
+	public int spawnOneItem;
 	
 	private string probError = ": Pickup List Array Length does not have assigned probabilities";
 	// Use this for initialization
@@ -21,21 +22,24 @@ public class PickupDropper: MonoBehaviour {
 		int numberOfDrops = rnd.Next(maxDrop);
 		if (debug_On)
 			numberOfDrops = 100;
-		for(int i = 0; i < numberOfDrops; i++)
-		{
-			int rand = rnd.Next(0, pickupList.Length + 1) - 1;
-			
-			if (rand == -1)
-			{
-				break;
-			}
-			else
-			{
-				GameObject itemToSpawn = FindSpawnItem(rand);
-				if (debug_On)
-					Debug.Log("Dropped an Item of type: " + itemToSpawn);
-				if (itemToSpawn != null)
-					Network.Instantiate(itemToSpawn, new Vector3(x, 1, z), Quaternion.identity, 0);
+
+		if (spawnOneItem != -1) {
+			GameObject itemToSpawn = FindSpawnItem (spawnOneItem);
+			Network.Instantiate (itemToSpawn, new Vector3 (x, 1, z), Quaternion.identity, 0);
+		} 
+		else {
+			for (int i = 0; i < numberOfDrops; i++) {
+				int rand = rnd.Next (0, pickupList.Length + 1) - 1;
+				
+				if (rand == -1) {
+					break;
+				} else {
+					GameObject itemToSpawn = FindSpawnItem (rand);
+					if (debug_On)
+						Debug.Log ("Dropped an Item of type: " + itemToSpawn);
+					if (itemToSpawn != null)
+						Network.Instantiate (itemToSpawn, new Vector3 (x, 1, z), Quaternion.identity, 0);
+				}
 			}
 		}
 		
