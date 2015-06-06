a02c9a8538c824adda41b4d1c82e96c3d63571b3
AnhQuan Nguyen <j4qfrost@gmail.com>
2015-06-05 21:34:09 -0700

    North wall and merge conflicts resolved

--- Unfold/Assets/Scripts/Pickups/PickupDropper.cs
+++ Unfold/Assets/Scripts/Pickups/PickupDropper.cs
@@ -23,7 +23,6 @@ public class PickupDropper: MonoBehaviour {
 		int numberOfDrops = rnd.Next(maxDrop + 1);
 		if (debug_On)
 			numberOfDrops = 100;
-<<<<<<< HEAD
 		for(int i = 0; i < numberOfDrops; i++)
 		{
 			Debug.Log ("Dropping stuff");
@@ -41,26 +40,7 @@ public class PickupDropper: MonoBehaviour {
 					Debug.Log("Dropped an Item of type: " + itemToSpawn);
 				if (itemToSpawn != null)
 					Network.Instantiate(itemToSpawn, new Vector3(x, 1, z), Quaternion.identity, 0);
-=======
 
-		if (spawnOneItem != -1) {
-			GameObject itemToSpawn = FindSpawnItem (spawnOneItem);
-			Network.Instantiate (itemToSpawn, new Vector3 (x, 1, z), Quaternion.identity, 0);
-		} 
-		else {
-			for (int i = 0; i < numberOfDrops; i++) {
-				int rand = rnd.Next (0, pickupList.Length + 1) - 1;
-				
-				if (rand == -1) {
-					break;
-				} else {
-					GameObject itemToSpawn = FindSpawnItem (rand);
-					if (debug_On)
-						Debug.Log ("Dropped an Item of type: " + itemToSpawn);
-					if (itemToSpawn != null)
-						Network.Instantiate (itemToSpawn, new Vector3 (x, 1, z), Quaternion.identity, 0);
-				}
->>>>>>> b595eecd92c198db60230723b6c68ca562cdadbc
 			}
 		}
 	}
